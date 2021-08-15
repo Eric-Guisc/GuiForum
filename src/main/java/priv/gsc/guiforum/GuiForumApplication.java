8b7149f4fb8835c373eaed65205b57254b3878d3
Eric-Guisc <1091877410@qq.com>
2021-08-15 16:59:34 +0800

    客户端前台页面

---
+++ src/main/java/priv/gsc/guiforum/GuiForumApplication.java
@@ -0,0 +1,15 @@
+package priv.gsc.guiforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class GuiForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuiForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/priv/gsc/guiforum/GuiForumApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.gsc.guiforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
